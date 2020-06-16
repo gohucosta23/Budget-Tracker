58f56b6e24834931284ce7f72fd2e9d0d686c057
hugo costa <gohucosta23@hotmail.com>
2020-06-16 14:26:43 -0400

    Fixing bug on Heroku connection

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 7000;
+const PORT = process.env.PORT || 7000;
 
 const app = express();
 
@@ -15,7 +15,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://user1:hello1@ds127954.mlab.com:27954/heroku_ccrtthlq", {
+var MONGODB_URI = process.env.MONGODB_URL || "mongodb://user1:hello1@ds127954.mlab.com:27954/heroku_ccrtthlq";
+
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
